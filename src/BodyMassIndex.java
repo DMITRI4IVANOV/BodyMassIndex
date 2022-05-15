9d14c1560bf34b5b12b967c30255d6663d008ccd
DMITRI4IVANOV <DmitryIvanovAN@yandex.ru>
2022-05-15 21:13:09 +0400

    BodyMassIn

--- src/BodyMassIndex.java
+++ src/BodyMassIndex.java
@@ -3,17 +3,7 @@ public class BodyMassIndex {
         int growth1 = growth * growth;
         int growth2 = growth1 / (int) 10000.0;
         int index = mass / growth2;
-        int a = (int) 18.5;
-         int b = 25;
-        if (index < a) {
-            System.out.println("Дефицит массы! ");
-        } else { if (index > b) {
-            System.out.println("Избыточный вес! ");
-        }
-         else { System.out.println("Норма! ");
-         }
-         }
         return index;
     }
-    }
+}
 
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ public class Main {
         BodyMassIndex service = new BodyMassIndex();
         int indexNormal = service.calculate(165, 50);
         System.out.println("Ваш индекс массы: " + indexNormal);
-
         int indexDeficit = service.calculate(180, 40);
         System.out.println("Ваш индекс массы: " + indexDeficit);
-
         int indexExcess = service.calculate(165, 80);
         System.out.println("Ваш индекс массы: " + indexExcess);
     }
